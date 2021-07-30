b94c9cafcee1d21f4fe61797479ac20c65004735
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-07-30 13:49:59 +0200

    Upgrade deps

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
