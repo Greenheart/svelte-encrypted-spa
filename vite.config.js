46aa138dfefaf6510eb623b96f25aa3b2a32abda
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-27 23:48:40 +0200

    Upgrade to SvelteKit 3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+import adapter from '@sveltejs/adapter-static'
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwind from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [sveltekit(), tailwind()],
+    plugins: [
+        sveltekit({
+            adapter: adapter({ fallback: 'index.html' }),
+            output: { bundleStrategy: 'inline' },
+            router: { type: 'hash' },
+        }),
+        tailwind(),
+    ],
 })
